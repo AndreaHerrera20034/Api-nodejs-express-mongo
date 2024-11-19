fea78144af5794fc05be74b22c3db9373ba17c4f
AndreaHerrera20034 <andreaherrera20034@gmail.com>
2024-11-18 19:03:16 -0600

    funcion del buscador

--- src/controllers/animeController.js
+++ src/controllers/animeController.js
@@ -4,7 +4,8 @@ const Anime = require('../models/anime');
 
 // Agregar o crear un nuevo anime a la categoria
 async function createAnime(req, res) {
-    const { title, genre, episodes, image } = req.body;
+    const { title, genre, episodes } = req.body;
+    const image = req.file ? req.file.filename : null; // Nombre del archivo subido
     try {
         const newAnime = new Anime({ title, genre, episodes, image });
         await newAnime.save();
@@ -38,6 +39,26 @@ async function getOneAnime(req, res) {
     }
 }
 
+// Obtener animes filtro de búsqueda
+async function getAnimesFilter(req, res) {
+    const { query } = req.query; // obtener el parametro de búsqueda desde la URL
+    try{
+        let animes = await Anime.find(); 
+
+        if(query){
+            const search = query.toLowerCase();
+            // Filtrar por titulo o genero que coincida parcialemente con la búsqueda
+            animes = animes.filter(anime =>
+                anime.title.toLowerCase().includes(search) ||
+                anime.genre.toLowerCase().includes(search)
+            );
+        }
+        res.json(animes); // Devolver los animes filtrados o todos si no hay query
+    } catch(error){
+        res.status(500).json({ message: 'Error al obtener los animes', error });
+    }
+}
+
 // Actualizar un anime
 async function updateAnime(req, res) {
     const { id } = req.params;
@@ -64,6 +85,7 @@ async function deleteAnime(req, res) {
 module.exports = {
     createAnime,
     getAnimes,
+    getAnimesFilter,
     getOneAnime,
     updateAnime,
     deleteAnime
